a90676cec4ee90b10bbdf536f5b5fc2a337fcf83
south-t <minami.linkc@gmail.com>
2023-06-06 13:23:02 +0900

    [Add] index

---
+++ IfSample.java
@@ -0,0 +1,13 @@
+package projava;
+public class IfSample{
+	public static void main(String[]args) {
+		var a = 4;
+		if (a < 3) {
+			System.out.println("小さい");
+		}else if(a < 7) {
+			System.out.println("中くらい");
+		}else {
+			System.out.println("大きい");
+		}
+	}
+}
---
+++ SwitchSample.java
@@ -0,0 +1,23 @@
+package projava;
+
+public class SwitchSample{
+	public static void main(String[]args){
+		var a = 1;
+		switch(a) {
+		case 1:
+		case 2:
+			System.out.println("one-two");
+		    
+		case 3:
+			System.out.println("three");
+			
+		case 4:
+			System.out.println("four");
+		    
+		default:
+			System.out.println("other");
+			
+	   }	
+	}
+}
+
